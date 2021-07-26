2ada4e737ba0bd7d66dff7dfdcf12742c8f50848
Luke Gartland <lukegartland@gmail.com>
2021-07-26 09:41:36 -1000

    min contrabution and payment requests

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -46,4 +46,27 @@ describe('Campaigns', () => {
         const isContributor = await campaign.methods.approvers(accounts[1]).call();
         assert(isContributor);
     })
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '5',
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (error) {
+            assert(error)
+        }
+    })
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods.createRequest('Buy batteries', '100', accounts[1])
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        const request = await campaign.methods.requests(0).call();
+
+        assert.strictEqual('Buy batteries', request.description)
+    })
 })

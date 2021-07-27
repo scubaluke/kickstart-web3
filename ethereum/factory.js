d997740c39a5d642236cf234c330cd2f7357b741
Luke Gartland <lukegartland@gmail.com>
2021-07-26 17:02:37 -1000

    web3 workaround for next.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,1 +1,6 @@
 import web3 from './web3'
+import CampaignFactory from './build/CampaignFactory.json'
+
+const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), process.env.deployedAddress)
+
+export default instance;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import factory from '../ethereum/factory'
 
-export default function newcampaign() {
+export default function CampaignIndex() {
+    useEffect(async () => {
+      const  campaigns = await factory.methods.getDeployedCampaigns().call()
+     console.log(campaigns);
+    }, [])
     return (
         <h1>
             this is the new campaign list page

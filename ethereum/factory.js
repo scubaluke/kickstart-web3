6d6a3aa4a875192936185287ec8305767e8cd120
Luke Gartland <lukegartland@gmail.com>
2021-07-27 16:52:21 -1000

    pulling contract data

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3 from './web3'
 import CampaignFactory from './build/CampaignFactory.json'
 
-const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), process.env.deployedAddress)
+const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), '0x665aABa468F47B0c34C9909009b11F190d002d46')
 
 export default instance;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import factory from '../ethereum/factory'
+// import React, { useState, useEffect } from 'react'
+// import factory from '../ethereum/factory'
 
-export default function CampaignIndex() {
-    useEffect(async () => {
-      const  campaigns = await factory.methods.getDeployedCampaigns().call()
-     console.log(campaigns);
-    }, [])
-    return (
-        <h1>
-            this is the new campaign list page
-        </h1>
-    )
+// function CampaignIndex() {
+//     const [campaigns, setCampaigns] = useState('initialState')
+
+//     useEffect(async () => {
+//       const  fetchedCampaigns = await factory.methods.getDeployedCampaigns().call()
+//       setCampaigns(fetchedCampaigns)
+//     }, [campaigns])
+//     return (
+//         <h1>
+//             {campaigns[0]}
+//         </h1>
+//     )
+// }
+
+// export default CampaignIndex;
+
+import React, { useState, useEffect } from "react";
+import factory from "../ethereum/factory";
+ 
+function CampaignIndex({ campaigns }) {
+  console.log("campaigns", campaigns);
+ 
+  return <h1>{campaigns[0]}</h1>;
 }
+ 
+//uses server side rendering to call the campaign contracts (so good for slow devices)
+CampaignIndex.getInitialProps = async () => {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return { campaigns };
+};
+ 
+export default CampaignIndex;

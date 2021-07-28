4de7f1b7bac63f7548edd99b58c260b92172fd4b
Luke Gartland <lukegartland@gmail.com>
2021-07-27 17:09:03 -1000

    server side render working

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/b16bb0aaa18a4383a6f68ab11f94350as"
+    "https://rinkeby.infura.io/v3/b16bb0aaa18a4383a6f68ab11f94350a"
   );
   web3 = new Web3(provider);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,13 @@
-// import React, { useState, useEffect } from 'react'
-// import factory from '../ethereum/factory'
-
-// function CampaignIndex() {
-//     const [campaigns, setCampaigns] = useState('initialState')
-
-//     useEffect(async () => {
-//       const  fetchedCampaigns = await factory.methods.getDeployedCampaigns().call()
-//       setCampaigns(fetchedCampaigns)
-//     }, [campaigns])
-//     return (
-//         <h1>
-//             {campaigns[0]}
-//         </h1>
-//     )
-// }
-
-// export default CampaignIndex;
-
 import React, { useState, useEffect } from "react";
 import factory from "../ethereum/factory";
  
 function CampaignIndex({ campaigns }) {
-  console.log("campaigns", campaigns);
  
   return <h1>{campaigns[0]}</h1>;
 }
  
 //uses server side rendering to call the campaign contracts (so good for slow devices)
-CampaignIndex.getInitialProps = async () => {
+CampaignIndex.getServerSideProps = async () => {
   const campaigns = await factory.methods.getDeployedCampaigns().call();
   return { campaigns };
 };

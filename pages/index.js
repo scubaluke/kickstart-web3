c5e8ab33a2dd48152e6e1a88412811e75ccbe8d4
Luke Gartland <lukegartland@gmail.com>
2021-07-27 17:07:33 -1000

    server side render working

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,16 @@
-// import React, { useState, useEffect } from 'react'
-// import factory from '../ethereum/factory'
+import React, { Component } from 'react'
+import factory from '../ethereum/factory'
 
-// function CampaignIndex() {
-//     const [campaigns, setCampaigns] = useState('initialState')
-
-//     useEffect(async () => {
-//       const  fetchedCampaigns = await factory.methods.getDeployedCampaigns().call()
-//       setCampaigns(fetchedCampaigns)
-//     }, [campaigns])
-//     return (
-//         <h1>
-//             {campaigns[0]}
-//         </h1>
-//     )
-// }
-
-// export default CampaignIndex;
-
-import React, { useState, useEffect } from "react";
-import factory from "../ethereum/factory";
- 
-function CampaignIndex({ campaigns }) {
-  console.log("campaigns", campaigns);
- 
-  return <h1>{campaigns[0]}</h1>;
+export default class CampaignIndex extends Component {
+   static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call()
+    return { campaigns }
+   }
+    render() {
+        return (
+            <div>
+                {this.props.campaigns[0]}
+            </div>
+        )
+    }
 }
- 
-//uses server side rendering to call the campaign contracts (so good for slow devices)
-CampaignIndex.getInitialProps = async () => {
-  const campaigns = await factory.methods.getDeployedCampaigns().call();
-  return { campaigns };
-};
- 
-export default CampaignIndex;

fcdad4e9a23afa7bd21390ee072c0c7c282c6039
Luke Gartland <lukegartland@gmail.com>
2021-07-28 16:02:08 -1000

    Layout and campaign card

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from "react";
 import factory from "../ethereum/factory";
- 
+import { Card, Button } from 'semantic-ui-react' 
+import Layout from '../components/Layout';
+import 'semantic-ui-css/semantic.min.css'; 
+
 function CampaignIndex({ campaigns }) {
+
+    const renderCampaigns = () => {
+        const items = campaigns.map(address => {
+            return {
+                header: address,
+                description: <a href="#">View Campaign</a>,
+                fluid: true
+            }
+        })
+        return <Card.Group items={items} />
+    }
  
-  return <h1>{campaigns[0]}</h1>;
+  return (
+      <Layout>
+          <div>
+            <h3>Open Campings</h3>
+            {renderCampaigns()}
+            <Button
+                content='Create Campaign'
+                icon='add circle'
+                primary 
+                />
+            </div>
+      </Layout>
+  );
 }
- 
-//uses server side rendering to call the campaign contracts (so good for slow devices)
-CampaignIndex.getServerSideProps = async () => {
+export default CampaignIndex;
+
+export const getServerSideProps = async () => {
   const campaigns = await factory.methods.getDeployedCampaigns().call();
-  return { campaigns };
+  return { props: {campaigns} };
 };
  
-export default CampaignIndex;

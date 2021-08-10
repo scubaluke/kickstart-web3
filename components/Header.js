eae2051603eff14f22a1aaecea7443670d1a406a
Luke Gartland <lukegartland@gmail.com>
2021-08-10 09:28:52 -1000

    deploy updates

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import { Menu } from 'semantic-ui-react'
 export default function Header() {
     return (
         <Menu style={{ marginTop: '10px' }} >
-            <Link route='/' ><a className='item' href="/">Krowd Coin</a></Link>
+            <Link route='/' ><a className='item' href="/">Crowd Coin</a></Link>
 
             <Menu.Menu position='right'>
                 <Link route='/'><a className='item' href="/">Campaigns</a></Link>
